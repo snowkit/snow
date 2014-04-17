15dfb70d0ed6c7b8c4fea67c56a60cadadafb5d0
Thomas Hourdel <chman.a11w@gmail.com>
2014-04-17 19:25:25 +0200

    Array buffers rewrite (a few critical bug fixed, added the missing types and functions to have a 1:1 mapping with JS/WebGL, except Float64Array).

--- lumen/utils/Float32Array.hx
+++ lumen/utils/Float32Array.hx
@@ -8,38 +8,25 @@ package lumen.utils;
 
     import lumen.utils.Matrix3D;
 
-
     class Float32Array extends ArrayBufferView implements ArrayAccess<Float> {
         
-        static public inline var SBYTES_PER_ELEMENT = 4;
+        public var BYTES_PER_ELEMENT (default, null) : Int;
+        public var length (default, null) : Int;
         
-        public var BYTES_PER_ELEMENT (default, null):Int;
-        public var length (default, null):Int;
-        
-        public function new (bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null) {
+        public function new( bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null ) {
             
             BYTES_PER_ELEMENT = 4;
 
-            if (Std.is (bufferOrArray, Int)) {
+            if (Std.is(bufferOrArray, Int)) {
                 
-                super (Std.int (bufferOrArray) * BYTES_PER_ELEMENT);
+                super(Std.int(bufferOrArray) << 2);
                 this.length = bufferOrArray;
                 
-            } else if (Std.is (bufferOrArray, Array)) {
+            } else if (Std.is(bufferOrArray, Array)) {
 
                 var floats:Array<Float> = bufferOrArray;
-                
-                if (length != null) {
-                    
-                    this.length = length;
-                    
-                } else {
-                    
-                    this.length = floats.length - start;
-                    
-                }
-                
-                super (this.length << 2);
+                this.length = (length != null) ? length : floats.length - start;
+                super(this.length << 2);
                 
                 #if !cpp
                 buffer.position = 0;
@@ -48,45 +35,92 @@ package lumen.utils;
                 for (i in 0...this.length) {
                     
                     #if cpp
-                    untyped __global__.__hxcpp_memory_set_float (bytes, (i << 2), floats[i]);
+                    untyped __global__.__hxcpp_memory_set_float(bytes, (i << 2), floats[i + start]);
                     #else
-                    buffer.writeFloat (floats[i + start]);
+                    buffer.writeFloat(floats[i + start]);
+                    #end
+                    
+                }
+                
+            } else if (Std.is(bufferOrArray, Float32Array)) {
+
+                var floats:Float32Array = bufferOrArray;
+                this.length = (length != null) ? length : floats.length - start;
+                super(this.length << 2);
+                
+                #if !cpp
+                buffer.position = 0;
+                #end
+                
+                for (i in 0...this.length) {
+                    
+                    #if cpp
+                    untyped __global__.__hxcpp_memory_set_float(bytes, (i << 2), floats[i + start]);
+                    #else
+                    buffer.writeFloat(floats[i + start]);
                     #end
                     
                 }
                 
             } else {
 
-                super (bufferOrArray, start, length << 2 );
+                super(bufferOrArray, start, length << 2);
                 
                 if ((byteLength & 0x03) > 0) {
-                    
-                    throw("Invalid array size");
-                    
+                    throw "Invalid array size";
                 }
                 
                 this.length = byteLength >> 2;
                 
                 if ((this.length << 2) != byteLength) {
-                    
                     throw "Invalid length multiple";
-                    
                 }
+
             }
-        }
-        
             
-        public function clear() {
-            length = 0;
+        }
+
+        public function set( bufferOrArray:Dynamic, offset:Int = 0 ) {
+
+            if (Std.is(bufferOrArray, Array)) {
+
+                var floats:Array<Float> = bufferOrArray;
+
+                for (i in 0...floats.length) {
+                    setFloat32((i + offset) << 2, floats[i]);
+                }
+            
+            } else if (Std.is(bufferOrArray, Float32Array)) {
+
+                var floats:Float32Array = bufferOrArray;
+
+                for (i in 0...floats.length) {
+                    setFloat32((i + offset) << 2, floats[i]);
+                }
+
+            } else {
+
+                throw "Invalid input buffer";
+
+            }
+
+        }
+
+        public function subarray( start:Int, end:Null<Int> = null ) : Float32Array {
+
+            end = (end == null) ? length : end;
+            return new Float32Array(buffer, start << 2, end - start);
+
         }
         
-        public static function fromMatrix (matrix:Matrix3D):Float32Array {
-            return new Float32Array (matrix.rawData);
+        public static function fromMatrix( matrix:Matrix3D ) : Float32Array {
+
+            return new Float32Array(matrix.rawData);
+
         }
         
-        @:noCompletion @:keep inline public function __get (index:Int):Float { return getFloat32 (index << 2); }
-        @:noCompletion @:keep inline public function __set (index:Int, value:Float):Void { setFloat32 (index << 2, value); }
-        
+        @:noCompletion @:keep inline public function __get( index:Int ):Float { return getFloat32(index << 2); }
+        @:noCompletion @:keep inline public function __set( index:Int, value:Float ) { setFloat32(index << 2, value); }
         
     }
 
