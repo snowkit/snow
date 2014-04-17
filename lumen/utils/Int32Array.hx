acd9ac5b2e077fecea4114515c25936d54e2a128
Thomas Hourdel <chman.a11w@gmail.com>
2014-04-17 19:25:25 +0200

    Array buffers rewrite (a few critical bug fixed, added the missing types and functions to have a 1:1 mapping with JS/WebGL, except Float64Array).

--- lumen/utils/Int32Array.hx
+++ lumen/utils/Int32Array.hx
@@ -4,43 +4,27 @@ package lumen.utils;
         
     typedef Int32Array = js.html.Int32Array;
 
-#end //lumen_html5
+#else
 
-#if lumen_native
-
-    @:arrayAccess
     class Int32Array extends ArrayBufferView implements ArrayAccess<Int> {
         
+        public var BYTES_PER_ELEMENT (default, null) : Int;
+        public var length (default, null) : Int;
         
-        static public inline var SBYTES_PER_ELEMENT = 4;
-        
-        public var BYTES_PER_ELEMENT (default, null):Int;
-        public var length (default, null):Int;
-        
-        
-        public function new (bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null) {
+        public function new( bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null ) {
             
             BYTES_PER_ELEMENT = 4;
             
-            if (Std.is (bufferOrArray, Int)) {
+            if (Std.is(bufferOrArray, Int)) {
                 
-                super (Std.int (bufferOrArray) << 2);
+                super(Std.int(bufferOrArray) << 2);
+                this.length = bufferOrArray;
                 
-            } else if (Std.is (bufferOrArray, Array)) {
+            } else if (Std.is(bufferOrArray, Array)) {
                 
                 var ints:Array<Int> = bufferOrArray;
-                
-                if (length != null) {
-                    
-                    this.length = length;
-                    
-                } else {
-                    
-                    this.length = ints.length - start;
-                    
-                }
-                
-                super (this.length << 2);
+                this.length = (length != null) ? length : ints.length - start;
+                super(this.length << 2);
                 
                 #if !cpp
                 buffer.position = 0;
@@ -49,39 +33,86 @@ package lumen.utils;
                 for (i in 0...this.length) {
                     
                     #if cpp
-                    untyped __global__.__hxcpp_memory_set_i32 (bytes, (i << 2), ints[i]);
+                    untyped __global__.__hxcpp_memory_set_i32(bytes, (i << 2), ints[i + start]);
                     #else
-                    buffer.writeInt (ints[i + start]);
+                    buffer.writeInt(ints[i + start]);
+                    #end
+                    
+                }
+                
+            } else if (Std.is(bufferOrArray, Int32Array)) {
+                
+                var ints:Int32Array = bufferOrArray;
+                this.length = (length != null) ? length : ints.length - start;
+                super(this.length << 2);
+                
+                #if !cpp
+                buffer.position = 0;
+                #end
+                
+                for (i in 0...this.length) {
+                    
+                    #if cpp
+                    untyped __global__.__hxcpp_memory_set_i32(bytes, (i << 2), ints[i + start]);
+                    #else
+                    buffer.writeInt(ints[i + start]);
                     #end
                     
                 }
                 
             } else {
                 
-                super (bufferOrArray, start, length);
+                super(bufferOrArray, start, length << 2);
                 
                 if ((byteLength & 0x03) > 0) {
-                    
-                    throw ("Invalid array size");
-                    
+                    throw "Invalid array size";
                 }
                 
                 this.length = byteLength >> 2;
                 
                 if ((this.length << 2) != byteLength) {
-                    
                     throw "Invalid length multiple";
-                    
                 }
                 
             }
             
         }
+
+        public function set( bufferOrArray:Dynamic, offset:Int = 0 ) {
+
+            if (Std.is(bufferOrArray, Array)) {
+
+                var ints:Array<Int> = bufferOrArray;
+
+                for (i in 0...ints.length) {
+                    setInt32((i + offset) << 2, ints[i]);
+                }
+            
+            } else if (Std.is(bufferOrArray, Int32Array)) {
+
+                var ints:Int32Array = bufferOrArray;
+
+                for (i in 0...ints.length) {
+                    setInt32((i + offset) << 2, ints[i]);
+                }
+
+            } else {
+
+                throw "Invalid input buffer";
+
+            }
+
+        }
+
+        public function subarray( start:Int, end:Null<Int> = null ) : Int32Array {
+
+            end = (end == null) ? length : end;
+            return new Int32Array(buffer, start << 2, end - start);
+
+        }
         
-        
-        @:noCompletion @:keep inline public function __get (index:Int):Int { return getInt32 (index << 2); }
-        @:noCompletion @:keep inline public function __set (index:Int, value:Int):Void { setInt32 (index << 2, value); }
-        
+        @:noCompletion @:keep inline public function __get( index:Int ):Int { return getInt32(index << 2); }
+        @:noCompletion @:keep inline public function __set( index:Int, value:Int ) { setInt32(index << 2, value); }
         
     }
 
