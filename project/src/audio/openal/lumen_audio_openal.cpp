eaf576e8f5a9e8f41b3cede0e30ef3f81f3e456f
underscorediscovery <sven@underscorediscovery.com>
2014-04-18 02:42:24 -0230

    AL, close to done, just bufferData and GetProcAddress and tests to go.

--- project/src/audio/openal/lumen_audio_openal.cpp
+++ project/src/audio/openal/lumen_audio_openal.cpp
@@ -107,21 +107,73 @@ namespace lumen {
 
     } DEFINE_PRIM(alhx_GetString, 1);
 
-    value alhx_GetBooleanv(value _param) {        
-        return alloc_null();  //:todo: 
-    } DEFINE_PRIM(alhx_GetBooleanv, 1);
+    value alhx_GetBooleanv(value _param, value _count) {
 
-    value alhx_GetIntegerv(value _param) {
-        return alloc_null();  //:todo: 
-    } DEFINE_PRIM(alhx_GetIntegerv, 1);
+        int count = val_int(_count);
+        ALboolean vals[count];
 
-    value alhx_GetFloatv(value _param) {
-        return alloc_null();  //:todo: 
-    } DEFINE_PRIM(alhx_GetFloatv, 1);
+        alGetBooleanv( val_int(_param), vals );
+        
+        value result = alloc_array(count);
 
-    value alhx_GetDoublev(value _param) {
-        return alloc_null();  //:todo: 
-    } DEFINE_PRIM(alhx_GetDoublev, 1);
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_bool(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetBooleanv, 2);
+
+    value alhx_GetIntegerv(value _param, value _count) {
+        
+        int count = val_int(_count);
+        ALint vals[count];
+
+        alGetIntegerv( val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_int(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetIntegerv, 2);
+
+    value alhx_GetFloatv(value _param, value _count) {
+
+        int count = val_int(_count);
+        ALfloat vals[count];
+
+        alGetFloatv( val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_float(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetFloatv, 2);
+
+    value alhx_GetDoublev(value _param, value _count) {
+        
+        int count = val_int(_count);
+        ALdouble vals[count];
+
+        alGetDoublev( val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_float((float)vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetDoublev, 2);
 
     value alhx_GetBoolean(value _param) {
         return alloc_bool( alGetBoolean(val_int(_param)) );
@@ -175,6 +227,14 @@ namespace lumen {
 
     value alhx_Listenerfv(value _param, value _values) {
         
+        float* vals = val_array_float(_values);
+
+        if(vals) {
+            alListenerfv( val_int(_param), vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
         return alloc_null();
 
     } DEFINE_PRIM(alhx_Listenerfv, 2);
@@ -197,14 +257,22 @@ namespace lumen {
 
     value alhx_Listeneriv(value _param, value _values) {
 
-        return alloc_null(); //:todo:
+        int *vals = val_array_int(_values);
+        
+        if(vals) {            
+            alListeneriv( val_int(_param), vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
 
     } DEFINE_PRIM(alhx_Listeneriv, 2);
-
 
     value alhx_GetListenerf(value _param) {
 
         float the_value;
+
         alGetListenerf( val_int(_param), &the_value );
 
         return alloc_float(the_value);
@@ -212,16 +280,42 @@ namespace lumen {
     } DEFINE_PRIM(alhx_GetListenerf, 1);
 
     value alhx_GetListener3f(value _param) {
-        return alloc_null(); //:todo:
+
+        ALfloat val1, val2, val3;
+
+        alGetListener3f( val_int(_param), &val1, &val2, &val3 );
+        
+        value result = alloc_array(3);
+
+            val_array_set_i(result, 0, alloc_float(val1) );
+            val_array_set_i(result, 1, alloc_float(val2) );
+            val_array_set_i(result, 2, alloc_float(val3) );
+
+        return result;
+
     } DEFINE_PRIM(alhx_GetListener3f, 1);
 
-    value alhx_GetListenerfv(value _param) {
-        return alloc_null(); //:todo:
-    } DEFINE_PRIM(alhx_GetListenerfv, 1);
+    value alhx_GetListenerfv(value _param, value _count) {
+
+        int count = val_int(_count);
+        ALfloat vals[count];
+
+        alGetListenerfv( val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_float(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetListenerfv, 2);
 
     value alhx_GetListeneri(value _param) {
 
         int the_value;
+
         alGetListeneri( val_int(_param), &the_value );
 
         return alloc_int(the_value);
@@ -229,12 +323,37 @@ namespace lumen {
     } DEFINE_PRIM(alhx_GetListeneri, 1);
 
     value alhx_GetListener3i(value _param) {
-        return alloc_null(); //:todo:
+
+        ALint val1, val2, val3;
+
+        alGetListener3i( val_int(_param), &val1, &val2, &val3 );
+        
+        value result = alloc_array(3);
+
+            val_array_set_i(result, 0, alloc_int(val1) );
+            val_array_set_i(result, 1, alloc_int(val2) );
+            val_array_set_i(result, 2, alloc_int(val3) );
+
+        return result;
+
     } DEFINE_PRIM(alhx_GetListener3i, 1);
 
-    value alhx_GetListeneriv(value _param) {
-        return alloc_null(); //:todo:
-    } DEFINE_PRIM(alhx_GetListeneriv, 1);
+    value alhx_GetListeneriv(value _param, value _count) {
+        
+        int count = val_int(_count);
+        ALint vals[count];
+
+        alGetListeneriv( val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_int(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetListeneriv, 2);
 
 // --- > not official api
 
@@ -261,15 +380,40 @@ namespace lumen {
 // --- > 
 
     value alhx_GenSources(value _n) {
-        return alloc_null(); //:todo:
+            
+        int count = val_int(_n);
+        ALuint sources[count];
+
+        alGenSources( count, sources );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_int(sources[i]) );
+        }
+
+        return result;
+
     } DEFINE_PRIM(alhx_GenSources, 1);
 
     value alhx_DeleteSources(value _n, value _sources) {
-        return alloc_null(); //:todo:
+        
+        int *sources = val_array_int(_sources);
+        
+        if(sources) {
+            alDeleteSources( val_int(_n), (ALuint*)sources );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_DeleteSources, 2);
 
     value alhx_IsSource(value _source) {
+
         return alloc_bool( alIsSource(val_int(_source)) );
+
     } DEFINE_PRIM(alhx_IsSource, 1);
 
 
@@ -290,7 +434,17 @@ namespace lumen {
     } DEFINE_PRIM(alhx_Source3f, 5);
 
     value alhx_Sourcefv(value _source, value _param, value _values) {
-        return alloc_null(); //:todo:
+
+        float *vals = val_array_float(_values);
+        
+        if(vals) {
+            alSourcefv( val_int(_source), val_int(_param), vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_Sourcefv, 3);
 
     value alhx_Sourcei(value _source, value _param, value _value) {
@@ -310,12 +464,23 @@ namespace lumen {
     } DEFINE_PRIM(alhx_Source3i, 5);
 
     value alhx_Sourceiv(value _source, value _param, value _values) {
-        return alloc_null(); //:todo:
+
+        int *vals = val_array_int(_values);
+        
+        if(vals) {
+            alSourceiv( val_int(_source), val_int(_param), vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_Sourceiv, 3);
 
     value alhx_GetSourcef(value _source, value _param) {
 
         float the_value;
+
         alGetSourcef( val_int(_source), val_int(_param), &the_value );
 
         return alloc_float(the_value);
@@ -323,16 +488,42 @@ namespace lumen {
     } DEFINE_PRIM(alhx_GetSourcef, 2);
 
     value alhx_GetSource3f(value _source, value _param) {
-        return alloc_null(); //:todo:
+
+        ALfloat val1, val2, val3;
+
+        alGetBuffer3f( val_int(_source), val_int(_param), &val1, &val2, &val3 );
+        
+        value result = alloc_array(3);
+
+            val_array_set_i(result, 0, alloc_float( val1 ) );
+            val_array_set_i(result, 1, alloc_float( val2 ) );
+            val_array_set_i(result, 2, alloc_float( val3 ) );
+
+        return result;
+
     } DEFINE_PRIM(alhx_GetSource3f, 2);
 
-    value alhx_GetSourcefv(value _source, value _param) {
-        return alloc_null(); //:todo:
+    value alhx_GetSourcefv(value _source, value _param, value _count) {
+    
+        int count = val_int(_count);
+        ALfloat vals[count];
+
+        alGetSourcefv( val_int(_source), val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_float(vals[i]) );
+        }
+
+        return result;
+
     } DEFINE_PRIM(alhx_GetSourcefv, 2);
 
     value alhx_GetSourcei(value _source, value _param) {
 
         int the_value;
+
         alGetSourcei( val_int(_source), val_int(_param), &the_value );
 
         return alloc_int(the_value);
@@ -340,28 +531,93 @@ namespace lumen {
     } DEFINE_PRIM(alhx_GetSourcei, 2);
 
     value alhx_GetSource3i(value _source, value _param) {
-        return alloc_null(); //:todo:
+        
+        ALint val1, val2, val3;
+
+        alGetSource3i( val_int(_source), val_int(_param), &val1, &val2, &val3 );
+        
+        value result = alloc_array(3);
+
+            val_array_set_i(result, 1, alloc_int( val1 ) );
+            val_array_set_i(result, 2, alloc_int( val2 ) );
+            val_array_set_i(result, 3, alloc_int( val3 ) );
+
+        return result;
+        
     } DEFINE_PRIM(alhx_GetSource3i, 2);
 
-    value alhx_GetSourceiv(value _source, value _param) {
-        return alloc_null(); //:todo:
-    } DEFINE_PRIM(alhx_GetSourceiv, 2);
+    value alhx_GetSourceiv(value _source, value _param, value _count) {
+        
+        int count = val_int(_count);
+        ALint vals[count];
+
+        alGetSourceiv( val_int(_source), val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_int(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetSourceiv, 3);
 
 
     value alhx_SourcePlayv(value _n, value _sources) {
-        return alloc_null(); //:todo:
+
+        int *vals = val_array_int(_sources);
+        
+        if(vals) {
+            alSourcePlayv( val_int(_n), (ALuint*)vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_SourcePlayv, 2);
 
     value alhx_SourceStopv(value _n, value _sources) {
-        return alloc_null(); //:todo:
+        
+        int *vals = val_array_int(_sources);
+        
+        if(vals) {
+            alSourceStopv( val_int(_n), (ALuint*)vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_SourceStopv, 2);
 
     value alhx_SourceRewindv(value _n, value _sources) {
-        return alloc_null(); //:todo:
+
+        int *vals = val_array_int(_sources);
+        
+        if(vals) {
+            alSourceRewindv( val_int(_n), (ALuint*)vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null(); 
+
     } DEFINE_PRIM(alhx_SourceRewindv, 2);
 
     value alhx_SourcePausev(value _n, value _sources) {
-        return alloc_null(); //:todo:
+        
+        int *vals = val_array_int(_sources);
+        
+        if(vals) {
+            alSourcePausev( val_int(_n), (ALuint*)vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_SourcePausev, 2);
 
 
@@ -399,24 +655,69 @@ namespace lumen {
 
 
     value alhx_SourceQueueBuffers(value _source, value _nb, value _buffers) {
-        return alloc_null(); //:todo:
+
+        int* buffers = val_array_int(_buffers);
+
+        if(buffers) {
+            alSourceQueueBuffers( val_int(_source), val_int(_nb), (ALuint*)buffers );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_SourceQueueBuffers, 3);
 
     value alhx_SourceUnqueueBuffers(value _source, value _nb, value _buffers) {
-        return alloc_null(); //:todo:
+        
+        int* buffers = val_array_int(_buffers);
+
+        if(buffers) {
+            alSourceUnqueueBuffers( val_int(_source), val_int(_nb), (ALuint*)buffers );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_SourceUnqueueBuffers, 3);
 
 
     value alhx_GenBuffers(value _n) {
-        return alloc_null(); //:todo:
+
+        int count = val_int(_n);
+        ALuint buffers[count];
+
+        alGenBuffers( count, buffers );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_int(buffers[i]) );
+        }
+
+        return result;
+
     } DEFINE_PRIM(alhx_GenBuffers, 1);
 
     value alhx_DeleteBuffers(value _n, value _buffers) {
-        return alloc_null(); //:todo:
+        
+        int *buffers = val_array_int(_buffers);
+        
+        if(buffers) {
+            alDeleteBuffers( val_int(_n), (ALuint*)buffers );
+        } else {
+            //todo: try val_array normal approach
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_DeleteBuffers, 2);
 
     value alhx_IsBuffer(value _buffer) {
+
         return alloc_bool( alIsBuffer(val_int(_buffer)) );
+
     } DEFINE_PRIM(alhx_IsBuffer, 1);
 
 //unofficial api 
@@ -443,6 +744,8 @@ namespace lumen {
 
 // // end unofficial api 
 
+    // :todo: bufferData(buffer:Int, format:Int, const ALvoid *data, size:Int, freq:Int)
+
     value alhx_Bufferf(value _buffer, value _param, value _value) {
 
         alBufferf(val_int(_buffer), val_int(_param), val_float(_value) );
@@ -460,7 +763,17 @@ namespace lumen {
     } DEFINE_PRIM(alhx_Buffer3f, 5);
 
     value alhx_Bufferfv(value _buffer, value _param, value _values) {
-        return alloc_null(); //:todo:
+        
+        float *vals = val_array_float(_values);
+        
+        if(vals) {
+            alBufferfv( val_int(_buffer), val_int(_param), vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_Bufferfv, 3);
 
     value alhx_Bufferi(value _buffer, value _param, value _value) {
@@ -480,7 +793,17 @@ namespace lumen {
     } DEFINE_PRIM(alhx_Buffer3i, 5);
 
     value alhx_Bufferiv(value _buffer, value _param, value _values) {
-        return alloc_null(); //:todo:
+            
+        int *vals = val_array_int(_values);
+        
+        if(vals) {
+            alBufferiv( val_int(_buffer), val_int(_param), vals );
+        } else {
+            //warn: try val_array normal approach?
+        }
+
+        return alloc_null();
+
     } DEFINE_PRIM(alhx_Bufferiv, 3);
 
     value alhx_GetBufferf(value _buffer, value _param) {
@@ -494,12 +817,37 @@ namespace lumen {
     } DEFINE_PRIM(alhx_GetBufferf, 2);
 
     value alhx_GetBuffer3f(value _buffer, value _param) {
-        return alloc_null(); //:todo:
+
+        ALfloat val1, val2, val3;
+
+        alGetBuffer3f( val_int(_buffer), val_int(_param), &val1, &val2, &val3 );
+        
+        value result = alloc_array(3);
+
+            val_array_set_i(result, 0, alloc_float( val1 ) );
+            val_array_set_i(result, 1, alloc_float( val2 ) );
+            val_array_set_i(result, 2, alloc_float( val3 ) );
+
+        return result;
+        
     } DEFINE_PRIM(alhx_GetBuffer3f, 2);
 
-    value alhx_GetBufferfv(value _buffer, value _param) {
-        return alloc_null(); //:todo:
-    } DEFINE_PRIM(alhx_GetBufferfv, 2);
+    value alhx_GetBufferfv(value _buffer, value _param, value _count) {
+        
+        int count = val_int(_count);
+        ALfloat vals[count];
+
+        alGetBufferfv( val_int(_buffer), val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_float(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetBufferfv, 3);
 
     value alhx_GetBufferi(value _buffer, value _param) {
         
@@ -512,12 +860,37 @@ namespace lumen {
     } DEFINE_PRIM(alhx_GetBufferi, 2);
 
     value alhx_GetBuffer3i(value _buffer, value _param) {
-        return alloc_null(); //:todo:
+        
+        ALint val1, val2, val3;
+
+        alGetBuffer3i( val_int(_buffer), val_int(_param), &val1, &val2, &val3 );
+        
+        value result = alloc_array(3);
+
+            val_array_set_i(result, 0, alloc_int( val1 ) );
+            val_array_set_i(result, 1, alloc_int( val2 ) );
+            val_array_set_i(result, 2, alloc_int( val3 ) );
+
+        return result;
+
     } DEFINE_PRIM(alhx_GetBuffer3i, 2);
 
-    value alhx_GetBufferiv(value _buffer, value _param) {
-        return alloc_null(); //:todo:
-    } DEFINE_PRIM(alhx_GetBufferiv, 2);    
+    value alhx_GetBufferiv(value _buffer, value _param, value _count) {
+        
+        int count = val_int(_count);
+        ALint vals[count];
+
+        alGetBufferiv( val_int(_buffer), val_int(_param), vals );
+        
+        value result = alloc_array(count);
+
+        for( int i = 0; i < count; ++i ) {
+            val_array_set_i(result, i, alloc_int(vals[i]) );
+        }
+
+        return result;
+
+    } DEFINE_PRIM(alhx_GetBufferiv, 3);
 
 
 //ALC
@@ -528,8 +901,9 @@ namespace lumen {
 
         if( Object_from_hx(_device, device) ) {
 
-                //second arg :todo:
-            ALCcontext *_al_context = alcCreateContext( device->al_device, NULL );
+            int* attrlist = val_array_int(_attrlist);
+
+            ALCcontext *_al_context = alcCreateContext( device->al_device, attrlist );
 
             if(!_al_context) {
                 printf("/ lumen / failed to create AL context\n");
@@ -695,14 +1069,23 @@ namespace lumen {
 
         if( Object_from_hx(_device, device) ) {
 
-            // :todo:
-            // alcGetIntegerv(device->al_device, val_int(_param), val_int(_size), &values) );
-            // return _array;
+            int count = val_int(_size);
+            ALint vals[count];
+
+            alcGetIntegerv( device->al_device, val_int(_param), count, vals );
+            
+            value result = alloc_array(count);
+
+            for( int i = 0; i < count; ++i ) {
+                val_array_set_i(result, i, alloc_int(vals[i]) );
+            }
+
+            return result;
 
         } //fetch device
 
         return alloc_null();
-        
+
     } DEFINE_PRIM(alhx_alcGetIntegerv, 3);
 
 
