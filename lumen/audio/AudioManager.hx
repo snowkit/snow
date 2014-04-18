f91f8d0fca6a1e7acb4dd5ea91673f8e3074cad0
underscorediscovery <sven@underscorediscovery.com>
2014-04-17 23:05:11 -0230

    Adding more openal features and some helpers

--- lumen/audio/AudioManager.hx
+++ lumen/audio/AudioManager.hx
@@ -3,25 +3,45 @@ package lumen.audio;
 import lumen.LumenTypes;
 
 import lumen.al.AL;
+import lumen.al.AL.Context;
+import lumen.al.AL.Device;
 
 class AudioManager {
 
     var lib : Lumen;
-    var device : Dynamic;
-    var context : Dynamic;
+
+    var device : Device;
+    var context : Context;
+    var source : Int;
 
     public function new( _lib:Lumen ) {
 
         lib = _lib;
 
-        device = AL.alcOpenDevice();
-        context = AL.alcCreateContext(device, null);
+        device = ALC.openDevice();
+        context = ALC.createContext(device, null);
 
+        ALC.makeContextCurrent( context );
 
+        source = AL.genSource();
+
+        AL.sourcef( source, AL.PITCH, 1 );
+
+        AL.sourcef( source, AL.GAIN, 1 );
+
+        AL.source3f( source, AL.POSITION, 0, 0, 0 );
+
+        AL.source3f( source, AL.VELOCITY, 0, 0, 0 );
+
+        AL.sourcei( source, AL.LOOPING, AL.FALSE );
+
+        trace( AL.getErrorMeaning(AL.getError()) );
     } //new
 
     public function destroy() {
-        AL.alcCloseDevice( device );
+        
+        ALC.closeDevice( device );
+
     }
 
     public function update() {
