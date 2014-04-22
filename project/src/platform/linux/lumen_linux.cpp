e12d0acb711a8d35814a2e2efd256193355d0ef1
underscorediscovery <sven@underscorediscovery.com>
2014-04-22 21:21:24 -0230

    adding platform specific hooks for init / update / shutdown

---
+++ project/src/platform/linux/lumen_linux.cpp
@@ -0,0 +1,20 @@
+#ifdef HX_LINUX
+
+namespace lumen {
+
+    void init_core_platform() {
+
+    } //init_core_platform
+
+    void shutdown_core_platform() {
+
+    } //shutdown_core_platform
+
+    void update_core_platform() {
+        
+    } //update_core_platform
+
+
+} //namespace lumen
+
+#endif
---
+++ project/src/platform/windows/lumen_windows.cpp
@@ -0,0 +1,22 @@
+#ifdef HX_WINDOWS
+
+
+namespace lumen {
+
+    
+    void init_core_platform() {
+
+    } //init_core_platform
+
+    void shutdown_core_platform() {
+
+    } //shutdown_core_platform
+
+    void update_core_platform() {
+        
+    } //update_core_platform
+
+
+} //namespace lumen
+
+#endif
