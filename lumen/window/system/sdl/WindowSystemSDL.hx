5d5cfae4483c4157f523b2d9c3e9d20e5b25d1ed
underscorediscovery <sven@underscorediscovery.com>
2014-04-23 06:34:30 -0230

    Adding note about SDL window system for now

--- lumen/window/system/sdl/WindowSystemSDL.hx
+++ lumen/window/system/sdl/WindowSystemSDL.hx
@@ -3,6 +3,9 @@ package lumen.window.system.sdl;
 
 import lumen.window.system.WindowSystem;
 
+//For now, there is no SDL specific window handling needed like audio/input handlers
+//So don't feel like this file is needlessly empty
+
 #if lumen_window_sdl
 
     class WindowSystemSDL extends WindowSystem {
