2ddbeb87def4befca61b66de19e9778a0024264d
underscorediscovery <sven@underscorediscovery.com>
2014-05-24 02:30:48 -0230

    Starting html5 implementation of the core features, timestamp, init, app_path, pref-path are done.
    Timestamp falls back on haxe.Timer.stamp() which seems to mimmick
    Date.now()/1000 in js… While if the browser supports performance.now()
    that is used instead

--- lumen/platform/html5/Core.hx
+++ lumen/platform/html5/Core.hx
@@ -6,13 +6,17 @@ import lumen.types.Types;
 @:noCompletion class Core extends lumen.Core.CoreBinding {
 
     public function new( _app:Lumen ) {
-        
         app = _app;
-
     } //new
 
         /** Called by the lumen internals to intiialize the core and subsystems of the framework, with the event handler passed in for where to deliver system events */
-    override public function init( _event_handler : SystemEvent->Void ) : Void {
+    override public function init( _event_handler : SystemEvent->Void ) : Void {        
+
+            //When we are done in here, start the main init procedure
+        app.dispatch_system_event({ type:SystemEventType.init });
+
+            //After it's had time to init, we fire the ready state
+        app.dispatch_system_event({ type:SystemEventType.ready });
 
     } //init
 
@@ -21,19 +25,23 @@ import lumen.types.Types;
 
     } //shutdown
 
-        /** Get the most precise timestamp available on the platform, in seconds (time is always in seconds in lumen) */
+        /** Get the most precise timestamp available on the platform, in seconds (time is always in seconds in lumen). */
     override public function timestamp() : Float {
-        return 0;
+        if(js.Browser.window.performance != null) {
+            return js.Browser.window.performance.now();
+        } else {
+            return haxe.Timer.stamp();
+        }
     } //timestamp
 
         /** Return the full path that the application is located */
     override public function app_path() : String {
-        return '';
+        return haxe.io.Path.directory(js.Browser.location.href) + '/';
     } //app_path
 
     //     /** On platforms where this makes sense, get the application specific writeable data path */
     override public function pref_path( _name:String, _package:String ) : String {
-        return '';
+        return './';
     } //pref_path
 
 
