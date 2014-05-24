9875c05f20cf54ea1a43c2c115c04f69c80e074a
underscorediscovery <sven@underscorediscovery.com>
2014-05-24 02:29:21 -0230

    Fixing AL defines being in the main packages as aliases

---
+++ lumen/audio/openal/AL.hx
@@ -0,0 +1,14 @@
+package lumen.audio.openal;
+
+#if lumen_html5
+	
+	//OpenAL doesn't exist on html5 platforms so, this should throw a compiler error even attempting to include this
+
+#else 
+
+	typedef AL = lumen.platform.native.audio.openal.AL;
+	typedef ALC = lumen.platform.native.audio.openal.AL.ALC;
+	typedef Device = lumen.platform.native.audio.openal.AL.Device;
+	typedef Context = lumen.platform.native.audio.openal.AL.Context;
+
+#end
