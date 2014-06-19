8a47733c7cba9272873ceaeaaeb9941b9962082a
underscorediscovery <sven@underscorediscovery.com>
2014-06-19 10:07:43 -0230

    fixing android builds breaking on platform code and namespaces

--- project/include/platform/android/lumen_android.h
+++ project/include/platform/android/lumen_android.h
@@ -8,34 +8,38 @@
 
 namespace lumen {
 
-    #ifdef __GNUC__
-      #define JAVA_EXPORT __attribute__ ((visibility("default"))) JNIEXPORT
-    #else
-      #define JAVA_EXPORT JNIEXPORT
-    #endif
+    namespace core {
 
-    struct AutoHaxe {
+        #ifdef __GNUC__
+            #define JAVA_EXPORT __attribute__ ((visibility("default"))) JNIEXPORT
+        #else
+            #define JAVA_EXPORT JNIEXPORT
+        #endif
 
-       int base;
-       const char *message;
+        struct AutoHaxe {
 
-       AutoHaxe(const char *inMessage) {
-          base = 0;
-          message = inMessage;
-          gc_set_top_of_stack(&base,true);
-          //__android_log_print(ANDROID_LOG_VERBOSE, "NME", "Enter %s %p", message, pthread_self());
-       }
+            int base;
+            const char *message;
 
-       ~AutoHaxe() {
-          //__android_log_print(ANDROID_LOG_VERBOSE, "NME", "Leave %s %p", message, pthread_self());
-          gc_set_top_of_stack(0,true);
-       }
+            AutoHaxe(const char *inMessage) {
+                base = 0;
+                message = inMessage;
+                gc_set_top_of_stack(&base,true);
+                //__android_log_print(ANDROID_LOG_VERBOSE, "NME", "Enter %s %p", message, pthread_self());
+            }
 
-    };
+            ~AutoHaxe() {
+                //__android_log_print(ANDROID_LOG_VERBOSE, "NME", "Leave %s %p", message, pthread_self());
+                gc_set_top_of_stack(0,true);
+            }
 
-    JNIEnv *GetEnv();
-    jclass FindClass(const char *className,bool inQuiet = false);
-    void CheckException(JNIEnv *env, bool inThrow=true);
+        };
+
+        JNIEnv *GetEnv();
+        jclass FindClass(const char *className,bool inQuiet = false);
+        void CheckException(JNIEnv *env, bool inThrow=true);
+
+    } //core namespace
 
 } //lumen namespace
 
