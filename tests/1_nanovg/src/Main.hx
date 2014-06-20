1281964f94ff1a857bfbb61194be723a9fa4c8a6
Michael Bickel <mib@developium.net>
2014-06-20 06:53:51 +0200

    added a font to nanovg-demo

--- tests/1_nanovg/src/Main.hx
+++ tests/1_nanovg/src/Main.hx
@@ -10,11 +10,13 @@ import lumen.render.opengl.GL;
 import hxnanovg.Nvg;
 using cpp.NativeString;
 
+
 @:buildXml("&<include name='${haxelib:hx-nanovg}/Build.xml'/>")
 class Main extends lumen.App {
 
     var vg:cpp.Pointer<NvgContext>;
-
+    var font:Int;
+    
     override function ready() {
 
         trace('/ HOST / ready');
@@ -25,11 +27,13 @@ class Main extends lumen.App {
 
         vg = Nvg.createGL(512, 512, NvgMode.ANTIALIAS);
 
+        font = Nvg.createFont(vg, "FiraSans-Regular".c_str(), "assets/FiraSans-Regular.ttf".c_str());
+
         app.window.render_handler = onrender;
 
     } //ready
 
-    override function onkeyup( keycode:Int, _,_, mod:ModState, _,_ ) {
+    override function onkeyup( keycode:Int, scancode:Int,_, mod:ModState, _,_ ) {
 
             //alt enter to toggle fullscreen test
         if( keycode == Key.RETURN && mod.alt ) {
@@ -47,6 +51,9 @@ class Main extends lumen.App {
 
     } //onkeyup
 
+    override function ontextinput( text:String, start:Int, length:Int, type:TextEventType, timestamp:Float, window_id:Int ) {
+        trace(text);
+    }
 
     override function update( delta:Float ) {
 
@@ -67,12 +74,27 @@ class Main extends lumen.App {
         GL.clearColor (0.3, 0.3, 0.3, 1.0);
         GL.clear (GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT | GL.STENCIL_BUFFER_BIT);
 
-        Nvg.beginFrame(vg, 800, 600, 1.0);
+        Nvg.beginFrame(vg, app.window.width, app.window.height, 1.0);
+        
+
+        Nvg.beginPath(vg);
         Nvg.rect(vg, 100,100, 500,300);
         Nvg.circle(vg, 120,120, 250);
         Nvg.pathWinding(vg, NvgSolidity.HOLE);   // Mark circle as a hole.
         Nvg.fillColor(vg, Nvg.rgba(255,192,0,255));
         Nvg.fill(vg);
+        
+        Nvg.fontFaceId(vg, font);
+        Nvg.fillColor(vg, Nvg.rgba(255,0,0,255));
+        Nvg.text(vg, 50, 50, "LOOOK SOME TEXT!!!".c_str(), untyped __cpp__("NULL"));
+        
+
+        Nvg.fontSize(vg, 200.0);
+        Nvg.fontFaceId(vg, font);
+        Nvg.fillColor(vg, Nvg.rgba(255,255,255,64));
+        Nvg.textAlign(vg, NvgAlign.ALIGN_LEFT|NvgAlign.ALIGN_MIDDLE);
+        Nvg.text(vg, 100, 100, "sdkfjdsh".c_str(), untyped __cpp__("NULL"));
+
         Nvg.endFrame(vg);
 
     } //onrender hook
