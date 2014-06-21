d26634660b8f32d3ea8217907db6e1edcbe691be
underscorediscovery <sven@underscorediscovery.com>
2014-06-21 07:46:42 -0230

    Adding initial unit tests for array buffers thanks to work from @Chman

---
+++ tests/_unit/src/buffers/TestInt32Array.hx
@@ -0,0 +1,85 @@
+package lumen.unit.buffers;
+
+import lumen.unit.UnitTest;
+import lumen.utils.ArrayBuffer;
+import lumen.utils.Int32Array;
+
+class TestInt32Array extends UnitTest
+{
+	public function test01()
+	{
+		var x = new Int32Array(2);
+		x[0] = 17;
+		assertEquals(17, x[0]);
+		assertEquals(0, x[1]);
+		assertEquals(2, x.length);
+	}
+	
+	public function test02()
+	{
+		var x = new Int32Array([17, -45]);
+		assertEquals(17, x[0]);
+		assertEquals(-45, x[1]);
+		assertEquals(2, x.length);
+	}
+	
+	public function test03()
+	{
+		var x = new Int32Array([17, -45]);
+		var y = new Int32Array(x.buffer);
+		assertEquals(17, x[0]);
+		assertEquals(-45, x[1]);
+		assertEquals(2, x.length);
+		x[0] = -2;
+		assertEquals(-2, y[0]);
+	}
+	
+	public function test04()
+	{
+		var x = new Int32Array([17, -45]);
+		var y = new Int32Array(x);
+		assertEquals(17, x[0]);
+		assertEquals(-45, x[1]);
+		assertEquals(2, x.length);
+		x[0] = -2;
+		assertEquals(17, y[0]);
+	}
+	
+	public function test05()
+	{
+		var buffer = new ArrayBuffer(12);
+		var x = new Int32Array(buffer, 0, 2);
+		var y = new Int32Array(buffer, 4, 1);
+		x[1] = 7;
+		assertEquals(7, y[0]);
+	}
+	
+	public function test06()
+	{
+		var x = new Int32Array(10);
+		x.set([18, 93, 42], 3);
+		assertEquals(18, x[3]);
+		assertEquals(93, x[4]);
+		assertEquals(42, x[5]);
+	}
+	
+	public function test07()
+	{
+		var x = new Int32Array(10);
+		x.set(new Int32Array([18, 93, 42]), 3);
+		assertEquals(18, x[3]);
+		assertEquals(93, x[4]);
+		assertEquals(42, x[5]);
+	}
+	
+	public function test08()
+	{
+		var x = new Int32Array([0, 1, 2, 3, 4, 5]);
+		var y = x.subarray(2, 5);
+		assertEquals(3, y.length);
+		assertEquals(2, y[0]);
+		assertEquals(3, y[1]);
+		y[0] = -1;
+		assertEquals(-1, x[2]);
+	}
+}
---
+++ tests/_unit/src/buffers/TestUInt8Array.hx
@@ -0,0 +1,85 @@
+package lumen.unit.buffers;
+
+import lumen.unit.UnitTest;
+import lumen.utils.ArrayBuffer;
+import lumen.utils.UInt8Array;
+
+class TestUInt8Array extends UnitTest
+{
+	public function test01()
+	{
+		var x = new UInt8Array(2);
+		x[0] = 17;
+		assertEquals(17, x[0]);
+		assertEquals(0, x[1]);
+		assertEquals(2, x.length);
+	}
+	
+	public function test02()
+	{
+		var x = new UInt8Array([17, -45]);
+		assertEquals(17, x[0]);
+		assertEquals(211, x[1]);
+		assertEquals(2, x.length);
+	}
+	
+	public function test03()
+	{
+		var x = new UInt8Array([17, -45]);
+		var y = new UInt8Array(x.buffer);
+		assertEquals(17, x[0]);
+		assertEquals(211, x[1]);
+		assertEquals(2, x.length);
+		x[0] = -2;
+		assertEquals(254, y[0]);
+	}
+	
+	public function test04()
+	{
+		var x = new UInt8Array([17, -45]);
+		var y = new UInt8Array(x);
+		assertEquals(17, x[0]);
+		assertEquals(211, x[1]);
+		assertEquals(2, x.length);
+		x[0] = -2;
+		assertEquals(17, y[0]);
+	}
+	
+	public function test05()
+	{
+		var buffer = new ArrayBuffer(12);
+		var x = new UInt8Array(buffer, 0, 6);
+		var y = new UInt8Array(buffer, 4, 1);
+		x[4] = 4;
+		assertEquals(4, y[0]);
+	}
+	
+	public function test06()
+	{
+		var x = new UInt8Array(10);
+		x.set([18, 93, 42], 3);
+		assertEquals(18, x[3]);
+		assertEquals(93, x[4]);
+		assertEquals(42, x[5]);
+	}
+	
+	public function test07()
+	{
+		var x = new UInt8Array(10);
+		x.set(new UInt8Array([18, 93, 42]), 3);
+		assertEquals(18, x[3]);
+		assertEquals(93, x[4]);
+		assertEquals(42, x[5]);
+	}
+	
+	public function test08()
+	{
+		var x = new UInt8Array([0, 1, 2, 3, 4, 5]);
+		var y = x.subarray(2, 5);
+		assertEquals(3, y.length);
+		assertEquals(2, y[0]);
+		assertEquals(3, y[1]);
+		y[0] = -1;
+		assertEquals(255, x[2]);
+	}
+}
