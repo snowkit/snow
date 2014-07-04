e408620e665a1f2977b947e664d240d0eada87b3
underscorediscovery <sven@underscorediscovery.com>
2014-07-04 16:18:33 -0230

    changing how window config is handled, instead of in the build file you configure the window before creating it from a get_window_config override, see 0_empty_window for an example.

--- flowtemplates/boot/SnowApp.hx
+++ flowtemplates/boot/SnowApp.hx
@@ -15,43 +15,10 @@ class SnowApp {
             //Create the app class, give it to the bootstrapper
         _host = new {{project.app.main}}();
 
-        //SDL_WINDOWPOS_UNDEFINED 0x1FFF0000;
-        //SDL_WINDOWPOS_CENTERED 0x2FFF0000;
-
-        var _window_config = null;
-
-        {{#with snow.window~}}
-
-                //Create the window config
-            _window_config = {
-
-                fullscreen : {{toString fullscreen}},
-                resizable : {{toString resizable}},
-                borderless : {{toString borderless}},
-                antialiasing : {{toString antialiasing}},
-                stencil_buffer : {{toString stencil}},
-                depth_buffer : {{toString depth}},
-                vsync : {{toString vsync}},
-                fps : {{fps}},
-
-                x               : Std.int(0x1FFF0000),
-                y               : Std.int(0x1FFF0000),
-                width           : {{width}},
-                height          : {{height}},
-                title           : "{{title}}",
-
-                orientation     : "{{orientation}}"
-
-            } //_window_config
-
-        {{~/with}}
-
             //Create the main config
         var _config : SnowConfig = {
 
             host            : _host,
-            window          : _window_config,
-            runtime         : {},
             run_loop        : true
 
         };
