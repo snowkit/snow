52c4dd1383d6681f746a48f3b751ad727bb175cd
underscorediscovery <sven@underscorediscovery.com>
2014-07-17 19:29:36 -0230

    Adding initial iOS project template files, generated correctly without errors, but not building yet

---
+++ flowtemplates/ios/Classes/Main.mm
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "./sdl/SDL_main.h"
+
+extern "C" const char *hxRunLibrary();
+extern "C" void hxcpp_set_top_of_stack();
+
+::foreach ndlls::
+ ::if (registerStatics)::
+     extern "C" int ::name::_register_prims();
+ ::end::
+::end::
+
+extern "C" int main(int argc, char *argv[]) {
+
+    //printf("Starting ...\n" );
+    hxcpp_set_top_of_stack();
+
+    ::foreach ndlls::
+     ::if (registerStatics)::
+      ::name::_register_prims();
+     ::end::
+    ::end::
+
+    const char *err = NULL;
+    err = hxRunLibrary();
+
+    if (err) {
+        printf(" Error %s\n", err );
+        return -1;
+    }
+
+    return 0;
+}
