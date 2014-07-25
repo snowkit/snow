e9333f16111831efac23037dda4e3e14a6f9e04b
underscorediscovery <sven@underscorediscovery.com>
2014-07-25 21:12:50 -0230

    Adding file dialogs for mac, and fixing inconsistency for file watch states (rename being called all the time etc).

--- project/src/platform/mac/snow_mac.mm
+++ project/src/platform/mac/snow_mac.mm
@@ -6,6 +6,9 @@
 
 #include <string>
 
+    //file dialogs
+#import <Cocoa/Cocoa.h>
+
 namespace snow {
 
     namespace core {
@@ -32,18 +35,85 @@ namespace snow {
 
     namespace platform {
 
-            //:todo:
+            //common helper,
+                //type 0 = open file
+                //type 1 = open folder
+        std::string open_select_path( int type, const std::string &title ) {
+
+            NSOpenPanel * panel = [NSOpenPanel openPanel];
+
+            [panel setAllowsMultipleSelection:NO];
+            [panel setFloatingPanel:YES];
+
+            if(type == 0) {
+                [panel setCanChooseDirectories:NO];
+                [panel setCanChooseFiles:YES];
+            } else if(type == 1) {
+                [panel setCanChooseDirectories:YES];
+                [panel setCanChooseFiles:NO];
+            }
+
+            [panel setTitle: [NSString stringWithCString:title.c_str() encoding:[NSString defaultCStringEncoding]] ];
+
+            // NSArray *fileTypes = [NSArray arrayWithObjects:@"ext1",@"ext2",nil];
+
+            NSInteger clicked = [ panel runModalForDirectory:NSHomeDirectory() file:nil types:nil ];
+
+            if(clicked == NSOKButton) {
+
+                NSString *chosen = [[panel URL] path];
+
+                    std::string result = std::string( [chosen UTF8String] );
+
+                [chosen release];
+
+                return result;
+
+            } //OK
+
+            return std::string();
+
+        } //open_select_path
+
         std::string dialog_folder(const std::string &title) {
-            return std::string("");
-        }
+
+            return open_select_path(1, title);
+
+        } //dialog_folder
 
         std::string dialog_open(const std::string &title) {
-            return std::string("");
-        }
+
+            return open_select_path(0, title);
+
+        } //dialog_open
 
         std::string dialog_save(const std::string &title) {
-            return std::string("");
-        }
+
+            NSSavePanel *panel = [NSSavePanel savePanel];
+
+            [panel setCanCreateDirectories:YES];
+            [panel setAllowsOtherFileTypes:YES];
+            [panel setExtensionHidden:YES];
+
+            [panel setTitle: [NSString stringWithCString:title.c_str() encoding:[NSString defaultCStringEncoding]] ];
+
+            NSInteger clicked = [panel runModal];
+
+            if (clicked == NSOKButton) {
+
+                NSString *chosen = [[panel URL] path];
+
+                    std::string result = std::string( [chosen UTF8String] );
+
+                [chosen release];
+
+                return result;
+
+            } //OK
+
+            return std::string();
+
+        } //dialog_save
 
     }
 
