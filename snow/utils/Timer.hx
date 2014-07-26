d83a0fd750eeeea84a0be24fa13b0bea2d05704a
underscorediscovery <sven@underscorediscovery.com>
2014-07-26 20:26:50 -0230

    Timer; unifying across targets to common code

--- snow/utils/Timer.hx
+++ snow/utils/Timer.hx
@@ -3,8 +3,6 @@ package snow.utils;
 import haxe.PosInfos;
 import haxe.Log;
 
-#if snow_native
-
     class Timer {
 
         static var running_timers:Array<Timer> = [];
@@ -75,86 +73,8 @@ import haxe.Log;
 
 
         static public function stamp():Float {
-            return snow_timestamp();
+            return Snow.core.timestamp();
         } //stamp()
 
-        static var snow_timestamp  = snow.utils.Libs.load( "snow", "snow_timestamp", 0 );
-
     } //Timer
 
-#else //snow_native
-
-    class Timer {
-
-        private var id : Null<Int>;
-
-            // Create a new timer that will run every [time_ms] (in milliseconds).
-        public function new( time_ms : Int ){
-            #if flash9
-                var me = this;
-                id = untyped __global__["flash.utils.setInterval"](function() { me.run(); },time_ms);
-            #elseif flash
-                var me = this;
-                id = untyped _global["setInterval"](function() { me.run(); },time_ms);
-            #elseif js
-                var me = this;
-                id = untyped window.setInterval(function() me.run(),time_ms);
-            #end
-        }
-
-            // Stop the timer definitely.
-        public function stop() {
-            if( id == null )
-                return;
-            #if flash9
-                untyped __global__["flash.utils.clearInterval"](id);
-            #elseif flash
-                untyped _global["clearInterval"](id);
-            #elseif js
-                untyped window.clearInterval(id);
-            #end
-            id = null;
-        }
-
-            // This is the [run()] method that is called when the Timer executes. It can be either overriden in subclasses or directly rebinded with another function-value.
-        public dynamic function run() {
-        }
-
-
-            // This will delay the call to [f] for the given time. [f] will only be called once.
-        public static function delay( time : Float, f : Void -> Void) {
-            var t = new haxe.Timer(time*1000);
-            t.run = function() {
-                t.stop();
-                f();
-            };
-            return t;
-        }
-
-
-            //Measure the time it takes to execute the function [f] and trace it. Returns the value returned by [f].
-        public static function measure<T>( f : Void -> T, ?pos : PosInfos ) : T {
-            var t0 = stamp();
-            var r = f();
-            Log.trace((stamp() - t0) + "s", pos);
-            return r;
-        }
-
-            // Returns the most precise timestamp, in seconds. The value itself might differ depending on platforms, only differences between two values make sense.
-        public static function stamp() : Float {
-            #if flash
-                return flash.Lib.getTimer() / 1000;
-            #elseif (neko || php)
-                return Sys.time();
-            #elseif js
-                return Date.now().getTime() / 1000;
-            #elseif cpp
-                return untyped __global__.__time_stamp();
-            #else
-                return 0;
-            #end
-        } //stamp
-
-    } //Timer
-
-#end //!native
