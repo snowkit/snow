1543c6dd48189bdd3132994ad0608c2e128510f3
underscorediscovery <sven@underscorediscovery.com>
2014-07-26 20:29:07 -0230

    removing old window config stuff

--- templates/html5/haxe/ApplicationMain.hx
+++ templates/html5/haxe/ApplicationMain.hx
@@ -15,37 +15,10 @@ class ApplicationMain {
             //Create the app class, give it to the bootstrapper
         _host = new ::APP_MAIN::();
 
-            //This is temporary because x/y are not passed in from lime-tools atm
-        //SDL_WINDOWPOS_UNDEFINED 0x1FFF0000;
-        //SDL_WINDOWPOS_CENTERED 0x2FFF0000;
-
-            //Create the window config
-        var _window_config = {
-
-            fullscreen      : ::WIN_FULLSCREEN::,
-            resizable       : ::WIN_RESIZABLE::,
-            borderless      : ::WIN_BORDERLESS::,
-            antialiasing    : Std.int(::WIN_ANTIALIASING::),
-            stencil_buffer  : ::WIN_STENCIL_BUFFER::,
-            depth_buffer    : ::WIN_DEPTH_BUFFER::,
-            vsync           : ::WIN_VSYNC::,
-            fps             : Std.int(::WIN_FPS::),
-            x               : Std.int(0x1FFF0000),
-            y               : Std.int(0x1FFF0000),
-            width           : Std.int(::WIN_WIDTH::),
-            height          : Std.int(::WIN_HEIGHT::),
-            title           : "::APP_TITLE::",
-
-            orientation     : "::WIN_ORIENTATION::"
-
-        } //_window_config
-
             //Create the main config
         var _config : SnowConfig = {
 
             host            : _host,
-            window          : _window_config,
-            runtime         : {},
             run_loop        : true
 
         };
