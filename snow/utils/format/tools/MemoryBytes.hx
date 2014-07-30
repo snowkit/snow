7f768b7b0397b562c0eea94b8a8359814e4c3ce4
underscorediscovery <sven@underscorediscovery.com>
2014-07-30 02:18:15 -0230

    utils; png; removing flash code as it conflicts when building docs

--- snow/utils/format/tools/MemoryBytes.hx
+++ snow/utils/format/tools/MemoryBytes.hx
@@ -10,13 +10,6 @@ extern class MemoryBytes {
 	inline function getPos() : Int {
 		return cast this;
 	}
-	
-	public inline function get( p : Int ) : Int {
-		return flash.Memory.getByte(getPos() + p);
-	}
 
-	public inline function set( p : Int, v : Int ) : Void {
-		flash.Memory.setByte(getPos() + p, v);
-	}
 	
 }
