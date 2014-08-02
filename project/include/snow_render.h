77c7da0d54c2e7fda2cad967332320d93a6b6d5e
underscorediscovery <sven@underscorediscovery.com>
2014-08-02 08:09:50 -0230

    render; add set context attributes, allowing the rendering to know what values it gets back. close #22

---
+++ project/include/snow_render.h
@@ -0,0 +1,16 @@
+#ifndef _SNOW_RENDER_H_
+#define _SNOW_RENDER_H_
+
+
+namespace snow {
+    namespace render {
+
+        void set_context_attributes(
+            int red_bits, int green_bits, int blue_bits, int alpha_bits,
+            int depth_bits, int stencil_bits, int antialiasing
+        );
+
+    } //render namespace
+} //snow namespace
+
+#endif //_SNOW_RENDER_H_
