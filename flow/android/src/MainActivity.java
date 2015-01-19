f3fb1d9288dbf6c1fd2bd9322916c2035afb3d5a
underscorediscovery <sven@underscorediscovery.com>
2015-01-19 06:44:50 -0330

    flow; android; update MainActivity.jar for user level java code, as well as SnowActivity for snow level JNI code and callbacks etc.
    Custom snow code will be migrated out of the SDL code, so simpler
    parity can be kept with updated SDL revisions.

--- flow/android/src/MainActivity.java
+++ flow/android/src/MainActivity.java
@@ -1,7 +1,27 @@
 package {{project.app.package}};
 
-import android.os.Bundle;
 
-public class MainActivity extends org.libsdl.app.SDLActivity {
-}
+public class MainActivity extends org.snowkit.snow.SnowActivity {
+
+    // 1. Override this file using flow,
+
+            // i.e
+            /*
+                if: {
+                  android: {
+                    build: {
+                      files: {
+                        activity : {
+                            path:"src/custom/MainActivity.java => project/src/MainActivity.java",
+                            template:'project'
+                        }
+                      }
+                    }
+                  }
+                }
+            */
+
+    // 2. Add custom Android Java here.
+
+} //MainActivity
 
