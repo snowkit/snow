e48dbc25ef66241b324c67f7bd380960e04bd4fc
underscorediscovery <sven@underscorediscovery.com>
2015-01-19 06:44:50 -0330

    flow; android; update MainActivity.jar for user level java code, as well as SnowActivity for snow level JNI code and callbacks etc.
    Custom snow code will be migrated out of the SDL code, so simpler
    parity can be kept with updated SDL revisions.

---
+++ flow/android/src/org/snowkit/snow/SnowActivity.java
@@ -0,0 +1,58 @@
+package org.snowkit.snow;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+
+public class SnowActivity extends org.libsdl.app.SDLActivity {
+
+    private final static String SNOW_TAG = "SNOW";
+    public static Activity snow_activity;
+    public native void nativeInit();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Create .....");
+        snow_activity = this;
+        nativeInit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Destroy .....");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Pause .....");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Restart .....");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Resume .....");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Start .....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(SNOW_TAG, ">>>>>>>>/ snow / On Stop .....");
+    }
+
+} //SnowActivity
+
