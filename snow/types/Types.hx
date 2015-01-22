0a4557d6928de8b836c858f7d235ef7e65ac90f1
underscorediscovery <sven@underscorediscovery.com>
2015-01-22 18:55:52 -0330

    Input; add prevent_default_keys as an array of int instead of a bool, allowing customizable page exclusive keys. Also made mouse wheel not scroll the page when targeting the canvas. fix underscorediscovery/luxe#203

--- snow/types/Types.hx
+++ snow/types/Types.hx
@@ -133,10 +133,11 @@ typedef AppConfigWeb = {
         /** If true, right clicking will consume the event on the canvas. `event.preventDefault` is used. default: true*/
     @:optional var no_context_menu : Bool;
 
-        /** If true, keys sent to the page will be consumed by snow. `event.preventDefault` is used. default: true*/
-    @:optional var prevent_default_keys : Bool;
+        /** Any Key.* values stored in this array sent to the page will be consumed by snow. `event.preventDefault` is used.
+            Keys can be removed or added to the array at runtime. default:left,up,down,right,backspace,tab,delete */
+    @:optional var prevent_default_keys : Array<Int>;
         /** If true, mouse wheel events sent to the page will be consumed by snow. `event.preventDefault` is used. default: true*/
-    @:optional var prevent_default_mouse : Bool;
+    @:optional var prevent_default_mouse_wheel : Bool;
 
         /** If true, native fullscreen will be requested from the user.
             If not, the canvas will fill the window size instead.
