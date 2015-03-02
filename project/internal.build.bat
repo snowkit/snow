9d99d46be088f0a7692e0dd1d302320edc15fb88
underscorediscovery <sven@underscorediscovery.com>
2015-03-02 18:42:53 -0330

    build; ci; default to clean builds for sanity

--- project/internal.build.bat
+++ project/internal.build.bat
@@ -9,7 +9,7 @@ git submodule update --init
 if defined SNOW_BUILD_ARCH_32 (
 
     echo "snow; build; running arch 32..."
-    haxelib run flow build --project project/snow.flow --arch 32 --d static_link --log %SNOW_BUILD_LOG_LEVEL%
-    haxelib run flow build --project project/snow.flow --arch 32 --d snow_dynamic_link --log %SNOW_BUILD_LOG_LEVEL%
+    haxelib run flow build --clean --project project/snow.flow --arch 32 --d static_link --log %SNOW_BUILD_LOG_LEVEL%
+    haxelib run flow build --clean --project project/snow.flow --arch 32 --d snow_dynamic_link --log %SNOW_BUILD_LOG_LEVEL%
 
 )
--- project/internal.build.sh
+++ project/internal.build.sh
@@ -12,8 +12,8 @@ if [ -n "$SNOW_BUILD_ARCH_64" ]
     then
 
     echo "snow; build; running arch 64..."
-    haxelib run flow build --project project/snow.flow --arch 64 --d static_link --log "$SNOW_BUILD_LOG_LEVEL"
-    haxelib run flow build --project project/snow.flow --arch 64 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean --project project/snow.flow --arch 64 --d static_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean --project project/snow.flow --arch 64 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
 
 fi
 
@@ -21,8 +21,8 @@ if [ -n "$SNOW_BUILD_ARCH_32" ]
 then
 
     echo "snow; build; running arch 32..."
-    haxelib run flow build --project project/snow.flow --arch 32 --d static_link --log "$SNOW_BUILD_LOG_LEVEL"
-    haxelib run flow build --project project/snow.flow --arch 32 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean --project project/snow.flow --arch 32 --d static_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean --project project/snow.flow --arch 32 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
 
 fi
 
@@ -30,9 +30,9 @@ if [ -n "$SNOW_BUILD_ANDROID" ]
 then
 
     echo "snow; build; running android archs armv6, armv7, x86 ..."
-    haxelib run flow build android --project project/snow.flow --arch armv6 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
-    haxelib run flow build android --project project/snow.flow --arch armv7 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
-    haxelib run flow build android --project project/snow.flow --arch x86 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean android --project project/snow.flow --arch armv6 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean android --project project/snow.flow --arch armv7 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean android --project project/snow.flow --arch x86 --d snow_dynamic_link --log "$SNOW_BUILD_LOG_LEVEL"
 
 fi
 
@@ -40,7 +40,7 @@ if [ -n "$SNOW_BUILD_IOS" ]
 then
 
     echo "snow; build; running android archs armv6, armv7, armv7s, arm64, sim, sim64 ..."
-    haxelib run flow build ios --project project/snow.flow --archs armv6,armv7,armv7s,arm64,sim,sim64 --d static_link --log "$SNOW_BUILD_LOG_LEVEL"
+    haxelib run flow build --clean ios --project project/snow.flow --archs armv6,armv7,armv7s,arm64,sim,sim64 --d static_link --log "$SNOW_BUILD_LOG_LEVEL"
 
 fi
 
