4c547850d1e0849603e62e7eb58fdbed16a978cc
underscorediscovery <sven@underscorediscovery.com>
2015-12-24 12:26:27 -0330

    Runtime; web; add config, runtime loop, window creation

--- snow/core/web/Runtime.hx
+++ snow/core/web/Runtime.hx
@@ -3,52 +3,57 @@ package snow.core.web;
 import snow.types.Types;
 import snow.api.Debug.*;
 
-typedef RuntimeConfig = {}
-typedef WindowHandle = js.html.CanvasElement;
-
 @:allow(snow.Snow)
 class Runtime implements snow.runtime.Runtime {
 
     public var app:snow.Snow;
     public var window : WindowHandle;
     public var name: String = 'web';
-    
+
+        //internal start time for 0 based time values
+    static var timestamp_start : Float = 0.0;
+
     function new(_app:snow.Snow) {
 
         app = _app;
-        timestamp_start = timestamp();
-
         app.platform = Platform.platform_web;
         app.os = guess_os();
 
+        timestamp_start = timestamp();
+
+        app.config.runtime = {
+            no_context_menu : true,
+            true_fullscreen : false,
+            prevent_default_mouse_wheel : true,
+            prevent_default_keys : [
+                Key.left, Key.right, Key.up, Key.down,
+                Key.backspace, Key.tab, Key.delete, Key.space
+            ]
+        };
+
+        log('web / init ok');
+
     } //new
 
-    function shutdown(?_immediate:Bool=false) {
+//Public API
 
-        log('runtime / web / shutdown');
+    public inline function window_device_pixel_ratio() {
 
-    } //shutdown
+        return js.Browser.window.devicePixelRatio == null ? 1.0 : js.Browser.window.devicePixelRatio;
 
-    function run() : Bool {
+    } //window_device_pixel_ratio
 
-        log('runtime / web / run');
+    public inline function window_width() : Int {
 
-        return false;
+        return window.height;
 
-    } //run
+    } //window_width
 
-    static var timestamp_start : Float = 0.0;
-    inline public static function timestamp() : Float {
+    public inline function window_height() : Int {
 
-        return (js.Browser.window.performance.now()/1000.0) - timestamp_start;
+        return window.height;
 
-    }
-
-    function ready() {
-
-        log('runtime / web / ready');
-        
-    } //ready
+    } //window_height
 
     public function window_grab(enable:Bool) : Bool {
 
@@ -66,28 +71,134 @@ class Runtime implements snow.runtime.Runtime {
 
     } //window_fullscreen
 
-    public function window_device_pixel_ratio() {
-        
-        var _dpr = js.Browser.window.devicePixelRatio;
-        if(_dpr == null) _dpr = 1.0;
-        
-        return _dpr;        
+//Public static API
 
-    } //window_device_pixel_ratio
+    inline public static function timestamp() : Float {
 
-    public function window_width() : Int {
+        return (js.Browser.window.performance.now()/1000.0) - timestamp_start;
 
-        return window.height;
+    }
 
-    } //window_width
+//Internal runtime API
 
-    public function window_height() : Int {
+    function shutdown(?_immediate:Bool=false) {
 
-        return window.height;
+        log('runtime / web / shutdown');
 
-    } //window_height
+    } //shutdown
+
+    function run() : Bool {
+
+        log('web / run');
+
+        request_frame();
+
+        return false;
+
+    } //run
+
+    function ready() {
+
+        log('web / ready');
+
+        create_window();
+
+    } //ready
 
 //internal
+
+    function create_window() {
+
+        var config = app.config.window;
+        var _dpr = window_device_pixel_ratio();
+
+        window = js.Browser.document.createCanvasElement();
+
+            //For High DPI, we scale the config sizes
+        window.width = Math.floor(config.width * _dpr);
+        window.height = Math.floor(config.height * _dpr);
+            //But keep them for the css pixels
+        window.style.width = config.width+'px';
+        window.style.height = config.height+'px';
+            //This is typically required for our WebGL and blending
+        window.style.background = '#000';
+
+        if(config.title != null && config.title != '') {
+            js.Browser.document.title = config.title;
+        }
+
+        var _parent = app.config.runtime.window_parent;
+        if(_parent == null) {
+            _parent = js.Browser.document.body;
+        }
+
+        _parent.appendChild(window);
+
+        if(!create_render_context(window)) {
+            throw Error.error('runtime / web / failed to create WebGL ${app.config.render.webgl.version} render context, unable to recover');
+        }
+
+    } //create_window
+
+    function create_render_context(_window:WindowHandle) : Bool {
+
+        var config = app.config.render;
+            //start with the user specified values, null otherwise
+        var attr : js.html.webgl.ContextAttributes = config.webgl;
+
+        attr.alpha = def(attr.alpha, false);
+        attr.premultipliedAlpha = def(attr.premultipliedAlpha, false);
+        attr.antialias = def(attr.antialias, app.config.render.antialiasing > 0);
+        attr.depth = def(attr.depth, app.config.render.depth > 0);
+        attr.stencil = def(attr.stencil, app.config.render.stencil > 0);
+
+        var _gl = null;
+        if(config.webgl.version != 1) {
+
+                //try the explicit version first,if that fails try experimental
+            var _contextid = 'webgl${config.webgl.version}';
+            _gl = window.getContext(_contextid);
+
+            if(_gl == null) {
+                _gl = window.getContext('experimental-$_contextid');
+            }
+
+        } else {
+            _gl = window.getContextWebGL(attr);
+        }
+
+        snow.modules.opengl.GL.gl = _gl;
+
+        log('web / GL / context($_gl)');
+
+        return _gl != null;
+
+    } //create_render_context
+
+    function apply_GL_attr(render:RenderConfig) {
+
+        log('web / GL / RBGA / request ${render.red_bits} ${render.green_bits} ${render.blue_bits} ${render.alpha_bits}');
+
+
+    } //apply_GL_attr
+
+    inline function request_frame() {
+
+        js.Browser.window.requestAnimationFrame(loop);
+
+    } //request_frame
+
+    function loop(?_t:Float = 0.016) : Bool {
+
+        app.dispatch_event(se_tick);
+
+        if(!app.shutting_down) {
+            request_frame();
+        }
+
+        return true;
+
+    } //loop
 
     function guess_os() {
 
@@ -114,3 +225,27 @@ class Runtime implements snow.runtime.Runtime {
     } //guess_os
 
 }
+
+
+typedef WindowHandle = js.html.CanvasElement;
+typedef RuntimeConfig = {
+
+        /** The js element to put the window canvas into. If null, <body> is used */
+    @:optional var window_parent : js.html.Element;
+
+        /** If true, right clicking will consume the event on the canvas. `event.preventDefault` is used. default: true*/
+    @:optional var no_context_menu : Bool;
+
+        /** Any Key.* values stored in this array sent to the page will be consumed by snow. `event.preventDefault` is used.
+            Keys can be removed or added to the array at runtime. default:left,up,down,right,backspace,tab,delete */
+    @:optional var prevent_default_keys : Array<Int>;
+        /** If true, mouse wheel events sent to the page will be consumed by snow. `event.preventDefault` is used. default: true*/
+    @:optional var prevent_default_mouse_wheel : Bool;
+
+        /** If true, native fullscreen will be requested from the user.
+            If not, the canvas will fill the window size instead.
+            Take note : true fullscreen requests only work when driven by a user event (click/keys).
+            You cannot force fullscreen on web. default: false */
+    @:optional var true_fullscreen : Bool;
+
+} //RuntimeConfig
